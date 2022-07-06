651825b5efe4629ce68604568bf3af97d67a93e5
dbsdlfud <99ysl@naver.com>
2022-07-06 11:02:57 +0900

    id지정해서 하나 출력

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -5,9 +5,11 @@ import com.mysite.sbb.article.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user/article")
@@ -21,9 +23,16 @@ public class ArticleController {
         return articleRepository.findAll();
     }
 
+//    @RequestMapping("/detail")
+//    @ResponseBody
+//    public Article showOne(){
+//        return articleRepository.findById(1L).get();
+//    }
+
     @RequestMapping("/detail")
     @ResponseBody
-    public Article showOne(){
-        return articleRepository.findById(1L).get();
+    public Article showNOne(@RequestParam long id, String name) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.orElse(null);
     }
 }
