23f34fc8d05c9b52dcfb870080fa612307b820d5
dbsdlfud <99ysl@naver.com>
2022-07-13 12:50:33 +0900

    html적용

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -3,11 +3,9 @@ package com.mysite.sbb.question.controller;
 import com.mysite.sbb.question.dao.QuestionRepository;
 import com.mysite.sbb.question.domain.Question;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,14 +14,12 @@ import java.util.List;
 @AllArgsConstructor
 public class QuestionController {
 
-    @Autowired
     private final QuestionRepository questionRepository;
 
     @RequestMapping("/list")
-    @ResponseBody
-    public List<Question> list(Model model){
+    public String showList(Model model){
         List<Question> questionList = this.questionRepository.findAll();
         model.addAttribute("questionList", questionList);
-        return questionRepository.findAll();
+        return "/user/list";
     }
 }
