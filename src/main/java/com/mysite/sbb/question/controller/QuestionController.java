fe95c7e141c7f2015b1f80133d12df4e2ec953cb
dbsdlfud <99ysl@naver.com>
2022-07-13 12:11:53 +0900

    진문 목록과 템플릿

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -2,8 +2,10 @@ package com.mysite.sbb.question.controller;
 
 import com.mysite.sbb.question.dao.QuestionRepository;
 import com.mysite.sbb.question.domain.Question;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,14 +13,17 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/question")
+@AllArgsConstructor
 public class QuestionController {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/list")
     @ResponseBody
-    public List<Question> list(){
+    public List<Question> list(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return questionRepository.findAll();
     }
 }
