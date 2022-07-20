fd4198710518d401c29b4a7772c45c064283da5d
dbsdlfud <99ysl@naver.com>
2022-07-20 12:42:22 +0900

    검증

--- src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
@@ -1,16 +1,18 @@
 package com.mysite.sbb.answer.controller;
 
-import com.mysite.sbb.answer.domain.Answer;
+import com.mysite.sbb.answer.AnswerForm;
 import com.mysite.sbb.answer.service.AnswerService;
 import com.mysite.sbb.question.domain.Question;
 import com.mysite.sbb.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/answer")
@@ -21,9 +23,14 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @RequestMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult){
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if(bindingResult.hasErrors()){ // 검증 실패
+            model.addAttribute("question", question);
+            return "/question_detail";
+        }
+        //model.addAttribute가 생략되어 있는거임
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 
@@ -34,4 +41,6 @@ public class AnswerController {
         this.answerService.setLike(answerId);
         return String.format("redirect:/question/detail/%s", questionId);
     }
+
+
 }
--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package com.mysite.sbb.question.controller;
 
+import com.mysite.sbb.answer.AnswerForm;
+import com.mysite.sbb.question.QuestionForm;
 import com.mysite.sbb.question.dao.QuestionRepository;
 import com.mysite.sbb.question.domain.Question;
 import com.mysite.sbb.question.service.QuestionService;
@@ -7,8 +9,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -27,7 +31,7 @@ public class QuestionController {
     }
 
     @RequestMapping("/detail/{id}")
-    public String showQuestion(Model model, @PathVariable("id") Integer id){
+    public String showQuestion(Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
 
@@ -36,13 +40,16 @@ public class QuestionController {
 
 
     @GetMapping("/create")
-    public String questionCreate(){ // 화면으로 이동
+    public String questionCreate(QuestionForm questionForm){ // 화면으로 이동
         return "/question_form";
     }
 
     @PostMapping("/create") // 데이터 보내서 등록
-    public String questionCreate(@RequestParam String subject, @RequestParam String content){
-        this.questionService.create(subject, content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){ // 검증에 실패한다면
+            return "/question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
